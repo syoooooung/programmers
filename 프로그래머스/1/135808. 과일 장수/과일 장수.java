42b244a2350a62c982a3b85ce40d8796fffce322
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-03-07 17:18:37 +0900

    [level 1] Title: 과일 장수, Time: 80.60 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        //사과 점수 1~k 점
+        //상자의 가장낮은 사과점수 * 사과 개수m
+        //m개씩 담아서 팔고 남은건 버림
+        Arrays.sort(score);
+        int idx=0, len = score.length;
+        int answer=0;
+        for(int i=len%m; i<len;i+=m){
+            answer+= m*score[i];
+        }
+        return answer;
+    }
+}
