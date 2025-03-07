875694acb6bca88496b8eff2982d53ee4b4129ad
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-03-07 18:20:47 +0900

    [level 1] Title: 옹알이 (2), Time: 8.83 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -2,22 +2,12 @@ import java.util.*;
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] can = {"aya", "ye", "woo", "ma"};
-        String[] cant = {"ayaaya", "yeye", "woowoo", "mama"};
         for(int i=0; i<babbling.length; i++){
-            for(int j=0; j<can.length; j++){
-                if(babbling[i].contains(cant[j])){
-                    babbling[i]=babbling[i].replace(cant[j],"1");
-                    break;
-                }
-                if(babbling[i].contains(can[j])){
-                    babbling[i]=babbling[i].replace(can[j],"0");
-                }
-            }
-            babbling[i]=babbling[i].replace("0","");
-            if(babbling[i].equals("")){
+            babbling[i]= babbling[i].replaceAll("ayaaya|yeye|woowoo|mama","1");
+            babbling[i]= babbling[i].replaceAll("aya|ye|woo|ma"," ");
+            babbling[i] = babbling[i].replaceAll(" ","");
+            if(babbling[i].equals(""))
                 answer++;
-            }
         }
         return answer;
     }
