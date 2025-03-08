6fe2e9ff51ba59e0f60a1ad6051b8f2584ab2d39
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-03-08 14:46:22 +0900

    Update 문자열 나누기.java

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,19 +1,16 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int x= s.charAt(0);
-        int samecnt=1;
-        for(int i=1; i<s.length(); i++){
+        char x =s.charAt(0);
+        int samecnt=0;
+        for(int i=0; i<s.length(); i++){
+            if(samecnt==0){
+                x= s.charAt(i);
+                answer++;
+            }
             char c = s.charAt(i);
             samecnt += c==x ? 1:-1;
-            if(samecnt==0){ //같은것과 다른것의 수가 같음
-                answer++;
-                if(i<s.length()-1)
-                    x= s.charAt(i+1);
-            }
         }
-        if(samecnt!=0)
-            answer++;
         return answer;
     }
 }
