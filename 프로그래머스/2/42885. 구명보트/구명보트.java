53f904b993938ee8fa2615dcd16727bc96ddf6db
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-03-10 18:08:24 +0900

    [level 2] Title: 구명보트, Time: 11.92 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -7,18 +7,14 @@ class Solution {
         for(int i=0 ; i<people.length ; i++){
             for(int j=findidx ; j>=i ; j--){
                 if(i==j){
-                    people[i]=-1;
                     answer++;
                 }
                 else if(people[i]+people[j]>limit){
-                    people[j]=-1;
                     answer++;
                     findidx=j-1;
                     continue;
                 }
                 else {
-                    people[i]=-1;
-                    people[j]=-1;
                     findidx=j-1;
                     answer++;
                     break;
