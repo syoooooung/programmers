c5109f8b766f3d978fc467e55c262248244afd73
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-03-20 21:12:25 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 18292 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] dp = new int[N+2];
+        dp[1]=1;
+        dp[2]=2;
+        //1 -> 1
+        //2 -> 00 11
+        //3 -> 000 001 100
+        //4 -> 0000 0011 1100 1001 1111
+        for(int i=3; i<=N; i++){
+            dp[i]=(dp[i-2]+dp[i-1])%15746;
+        }
+        System.out.println(dp[N]);
+    }
+
+}
