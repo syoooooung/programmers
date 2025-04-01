0663230cfbcce481c180851cbc1c767093122ff4
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-04-01 16:01:25 +0900

    [level 2] Title: 괄호 회전하기, Time: 56.84 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,53 +1,45 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        int len = s.length();
-        String[] sqrs = {"({[","]})"};
-        int[] nums=new int[3];
-        Stack<Integer> st = new Stack<>(); //괄호index저장
-        st.push(9);
-        String nextS = s;
-        boolean breakS=false;
-        for(int i=0; i<s.length(); i++){ //string 길이만큼 반복
-            breakS=false;
-            for(char c : nextS.toCharArray()){
-                boolean open = sqrs[0].contains(String.valueOf(c));
-                int idx = index(c);
-                if( !open && idx !=st.peek() && st.peek()%2 == 0){ 
-                    st.push(idx);
-                    nums[idx]++;
-                }
-                else if( !open && idx==st.peek() ){ 
-                    st.pop();
-                    nums[idx]--;
-                }else if( open  ){
-                    st.push(idx);
-                    nums[idx]++;
-                }
-                else{
-                    breakS=true;
-                    break;
-                }
-                //System.out.println(st+"====");
-            }
-            if(!breakS &&st.size()==1 ){
-                answer++;
-                //System.out.println("answer plus");
-            }
-            st.clear(); st.push(9);
-            nextS = nextS.substring(len-1,len)+nextS.substring(0,len-1); //한바퀴 돌리기
-            //System.out.println("=====NEXT=====");
+        //일단 올바른지 판단은 어떻게 할거냐
+        //내가 닫으려는데 내가 아닌 다른놈이 벌리고 있으면 false임
+        //stack에 넣으면서 닫히면 pop해주기
+        int ans=0;
+        for(int i=0; i<s.length() ; i++){
+            if(isValid(s))
+                ans++;
+            s=s.substring(s.length()-1)+s.substring(0,s.length()-1);
         }
-        return answer;
+        return ans;
     }
-    static private int index(char c){
-        if(c == '(' || c==')')
-            return 0;
-        if(c =='{' || c=='}')
-            return 1;
-        if(c=='[' || c==']')
-            return 2;
-        return -1;
+    static private boolean isValid(String s){
+        //새로 여는거면 ㄱㄴ
+        //닫을거면 1. 내가 열고있거나, 2.비어있으면안됨
+        Stack<String> st = new Stack<>();
+        String opens="[{("; int idx=0;
+        for(int i=0; i<s.length() ; i++){
+            String x = s.substring(i,i+1);
+            if(opens.contains(x)){ 
+                st.push(x); continue;
+            }
+            switch (s.charAt(i)){
+                case ']':
+                    idx=0;
+                    break;
+                case '}':
+                    idx=1;
+                    break;
+                case ')':
+                    idx=2;
+                    break;
+                default:
+                    break;
+            }
+            if(st.empty() || opens.charAt(idx)!=st.peek().charAt(0)){
+                return false;
+            }
+            st.pop();
+        }
+        return st.empty()?true:false;
     }
 }
