c8c8a9b5d6ef66cbe643d53b6c6648003b597252
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-04-01 15:07:00 +0900

    [level 2] Title: 예상 대진표, Time: 0.09 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,16 +2,15 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 1;
-        int max= a>b?a:b;
-        int min= a<b? a:b;
-        while(min+1 != max || min%2!=1){
-            min =(int) (min+1)/2;
-            max =(int) (max+1)/2;
+        int min = a<b? a:b;
+        int max = a<b? b:a;
+        //가각 1을 더해서 2로 나눈거로 올라가게됨
+        int answer=1;
+        while(min+1!=max || max%2!=0){
+            min= (min+1)/2;
+            max = (max+1)/2;
             answer++;
         }
-        
-
         return answer;
     }
 }
