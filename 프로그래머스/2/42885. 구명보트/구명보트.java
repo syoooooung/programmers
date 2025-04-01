ad33ea124cae6614ed2e577a1029d8b9c29036d2
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-04-01 14:05:42 +0900

    [level 2] Title: 구명보트, Time: 14.98 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,28 +1,19 @@
 import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
-        int findidx = people.length-1;
+        int ans=0, idx=people.length -1;
         Arrays.sort(people);
-        for(int i=0 ; i<people.length ; i++){
-            for(int j=findidx ; j>=i ; j--){
-                if(i==j){
-                    answer++;
-                }
-                else if(people[i]+people[j]>limit){
-                    answer++;
-                    findidx=j-1;
-                    continue;
-                }
-                else {
-                    findidx=j-1;
-                    answer++;
-                    break;
-                }
-                
+        //people 배열에서 태운 사람들은 -1로 하고
+        for(int i=0; i<=idx ; i++){
+            while(people[i]+people[idx]>limit && idx>i){
+                ans++; idx--;
             }
-            
+            ans++; //루프 탈출을 햇다는건 i까지온거거나 limit 보다 작은거거나
+            //i까지 왔을경우 -> ans++하고 rbeak;
+            //limit보다 작을경우 -> ans++ 하고 idx-- 해주고 break;
+            if(i==idx){break;}
+            idx--;
         }
-        return answer;
+        return ans;
     }
 }
