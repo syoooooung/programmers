5cb3883dc73a83cc1b17e56cc3a1b59cc1f6a4d2
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-04-01 11:57:34 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.68 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -3,13 +3,13 @@ class Solution
 {
     public int solution(int []A, int []B)
     {
-        int answer = 0;
+        //두 수의 곱이 최소가 되게끔 (최대와 최소를 곱하면돼는데)
         Arrays.sort(A);
         Arrays.sort(B);
+        int ans=0;
         for(int i=0; i<A.length ; i++){
-            answer+= A[i]*B[B.length-i-1];
+            ans += A[i]*B[B.length-i-1];
         }
-
-        return answer;
-    }
+        return ans;
+    }    
 }
