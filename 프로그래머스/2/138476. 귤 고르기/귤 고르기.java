8548e7b4f5d37bf06e08adbd9df276d614c1dc44
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-04-01 14:22:35 +0900

    [level 2] Title: 귤 고르기, Time: 84.32 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,24 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        //서로 다른 종류가 최소
+        //종류가 최소가 되도록 k개를 한상자에 담기
+        int ans=0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int t:tangerine){
-            map.put(t,map.getOrDefault(t,0)+1);
+        for(int i:tangerine){
+            map.put(i, map.getOrDefault(i,0)+1);
         }
-        int[][] arr = new int[map.size()][2];
-        int idx=0;
-        for(int key: map.keySet()){
-            arr[idx][0]=key;
-            arr[idx][1]=map.get(key);
-            idx++;
+        List<Integer> list= new ArrayList<>(map.keySet());
+        list.sort((e1,e2)->map.get(e2)-map.get(e1));
+        for(int i=0; k>0 ; i++){
+            k-=map.get(list.get(i));
+            ans++;
         }
-        Arrays.sort(arr, Comparator.comparingInt((int[] o)->o[1]).reversed());
-        for(int i=0; i<idx && k>0 ; i++){
-            k-= arr[i][1];
-            answer++;
-        }
-        return answer;
+        return ans;
     }
 }
