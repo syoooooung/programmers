8725c3aa878448d102318c8c6e1dc0f7e61a285e
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-04-01 14:36:19 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,11 +1,10 @@
 class Solution {
     public long solution(int n) {
-        long answer = 0;
-        long[] dp = new long[n+2];
-        dp[1]=1;
-        dp[2]=2;
+        long[] dp= new long[n+2];
+        dp[1]=1;dp[2]=2;
         for(int i=3; i<=n ; i++){
-            dp[i]= (dp[i-1]+dp[i-2])%1234567;
+            dp[i]=  (dp[i-1]+ dp[i-2])%1234567; //이전칸에서 1칸오는 경우의 수와
+                //이이전 칸에서 2칸넘어오는 경우의 수가 있음   
         }
         return dp[n];
     }
