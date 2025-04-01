0d237cc65c323e58cc1033753698855985640c82
syoooooung <110389611+syoooooung@users.noreply.github.com>
2025-04-01 11:47:18 +0900

    [level 2] Title: 올바른 괄호, Time: 5.13 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,13 +1,12 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        int open=0;
+        int isTrue=0;
         for(int i=0; i<s.length(); i++){
-            open+= s.charAt(i)=='('? 1:-1;
-            if(open<0)
+            isTrue += s.charAt(i)=='('? 1: -1;
+            if(isTrue<0){
                 return false;
+            }
         }
-        answer = open==0 ? true: false;
-        return answer;
+        return isTrue==0? true: false;
     }
 }
